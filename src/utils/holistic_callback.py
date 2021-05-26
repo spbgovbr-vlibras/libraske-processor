a7c7828deb2835c3143323e17e6d4c9773cf2cfb
Ewerton Moura <emoura@lavid.ufpb.br>
2021-05-26 08:33:56 -0300

    remove base64 encode

--- src/utils/holistic_callback.py
+++ src/utils/holistic_callback.py
@@ -18,27 +18,29 @@ class HolisticCallback():
         frame_id = message["frame_id"]
         frame_link = message["frame_link"]
 
-        with open(frame_link, "rb") as f:
-            im_b64 = base64.b64encode(f.read())
+        # with open(frame_link, "rb") as f:
+        #     im_b64 = base64.b64encode(f.read())
 
-        im64_bytes = base64.b64decode(im_b64)
+        # im64_bytes = base64.b64decode(im_b64)
 
-        im_arr = np.frombuffer(im64_bytes, dtype=np.uint8)
-        img = cv2.imdecode(im_arr, flags=cv2.COLOR_BGR2RGB)
+        # im_arr = np.frombuffer(im64_bytes, dtype=np.uint8)
+        # img = cv2.imdecode(im_arr, flags=cv2.COLOR_BGR2RGB)
         file_id = str(uuid4())[:5]
         mp_drawing = mp.solutions.drawing_utils
         mp_holistic = mp.solutions.holistic
 
         with mp_holistic.Holistic(static_image_mode=True) as holistic:
 
-            results = holistic.process(img)
+            image = cv2.imread(frame_link)
+
+            results = holistic.process(image)
             if results.pose_landmarks:
                 # print(
                 #     f'Nose coordinates: ('
                 #     f'{results.pose_landmarks.landmark}, '
                 #     f'{results.pose_landmarks.landmark})'
                 # )
-                annotated_image = img.copy()
+                annotated_image = image.copy()
                 mp_drawing.draw_landmarks(
                     annotated_image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
                 mp_drawing.draw_landmarks(
